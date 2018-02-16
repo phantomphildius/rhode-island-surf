1d127b9f6da95f8245f23668578e6b7dac1c7013
Andrew Freeman <afreeman@simplereach.com>
2018-02-16 18:33:59 -0500

    Add location drop down

---
+++ src/Container.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import Dashboard from './Dashboard';
+import Select from 'material-ui/Select';
+import { MenuItem } from 'material-ui/Menu';
+
+class Container extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      locationId: 846,
+      locationName: 'Second Beach'
+    }
+  }
+
+  handleChange = event =>  {
+    this.setState({
+      locationId: event.target.value,
+      locationName: event.target.name
+    })
+  }
+
+  render() {
+    return (
+      <div>
+      <Select
+       value={this.state.locationId}
+       onChange={this.handleChange}
+      >
+        <MenuItem value={846}>Second Beach</MenuItem>
+        <MenuItem value={907}>First Beach</MenuItem>
+        <MenuItem value={374}>Ruggles</MenuItem>
+      </Select>
+      // {this.state.locationName}
+      <Dashboard name="Swell Height" spotId={this.state.locationId} fields='swell.components.combined.height' />
+      <Dashboard name="Temperature" spotId={this.state.locationId} fields='condition.temperature' />
+      <Dashboard name="Wind Speed" spotId={this.state.locationId} fields='wind.speed' />
+      {this.state.locationId}
+      </div>
+    )
+  }
+}
+
+export default Container;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Dashboard from './Dashboard';
+import Container from './Container';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-  <div>
-    <Dashboard name="Swell Height" spotId='846' fields='swell.components.combined.height' />
-    <Dashboard name="Temperature" spotId='846' fields='condition.temperature' />
-    <Dashboard name="Wind Speed" spotId='846' fields='wind.speed' />
-  </div>
+  <Container />
   , document.getElementById('root')
 );
 registerServiceWorker();
-    // <Dashboard name="First Beach" spotId='907' fields='localTimestamp,swell.components.combined.*,wind.*,condition.temperature' />
-    // <Dashboard name ="Ruggles" spotId='374' fields='localTimestamp,swell.components.combined.*,wind.*,condition.temperature' />
